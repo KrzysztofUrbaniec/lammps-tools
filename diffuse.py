3582e8feba9a5ad5b44fc929eef94dbd8a4eec8a
Krzysztof Urbaniec <urbaniec.krzysztof@interia.pl>
2023-08-14 08:53:56 +0200

    Changed calculate_diffusion_coefficient function
    Now it uses OLS model from statsmodels and additionally returns the slope and the error of fit

--- diffuse.py
+++ diffuse.py
@@ -47,37 +47,43 @@ def compute_MSD(moltype_obj: DumpFileLoader.MoleculeType, n_origin=None, as_key=
         return MSD    
 
 
-def calculate_diffusion_coefficient(moltype_obj, start_time=None, end_time=None):
+def calculate_diffusion_coefficient(moltype_obj, start_time=None, end_time=None, find_best_interval_flag=True):
     MSD = moltype_obj.data_dict['MSD'].mean(axis=1)
     dump_freq = moltype_obj.timesteps[1] 
     time = np.arange(1, MSD.shape[0]+1) * dump_freq
 
-    start_time, end_time = find_best_interval(moltype_obj)
+    if find_best_interval_flag:
+        # Find the best interval based on log-log plot slope
+        start_time, end_time = find_best_interval(moltype_obj)
+    else:
+        # If both start_time and end_time are None, use entire time range (usually not a good idea)
+        if end_time is None: end_time = len(time) + 1
+        if start_time is None: start_time = 0
     
-    # # If both start_time and end_time are None, use entire time range (usually not a good idea)
-    # if end_time is None: end_time = len(time) + 1
-    # if start_time is None: start_time = 0
-    
-    lin_reg = LinearRegression()
-    lin_reg.fit(time[start_time:end_time].reshape(-1,1), MSD[start_time:end_time].reshape(-1,1))
+    time_interval = time[start_time:end_time]
+    MSD_interval = MSD[start_time:end_time]
 
-    diffusion_coefficient = lin_reg.coef_ / 6 * 1e15 * 1e-20 # m^2/s
-    return diffusion_coefficient    
+    # Build ordinary least squares model
+    time_interval_stats = sm.add_constant(time_interval)
+    model = sm.OLS(MSD_interval, time_interval_stats)
+    results = model.fit()
+    
+    slope = results.params[1]
+    slope_fit_error = results.bse[1]
+    diffusion_coefficient = slope / 6 * 1e15 * 1e-20 # m^2/s
+    return (diffusion_coefficient, slope, slope_fit_error)
 
 def find_best_interval(moltype_obj):
     MSD = moltype_obj.data_dict['MSD'].mean(axis=1)
     dump_freq = moltype_obj.timesteps[1] 
     time = np.arange(1, MSD.shape[0]+1) * dump_freq
-    # logging.debug(f'Time[-1]: {time[-1]}')
 
     MSD_log = np.log10(MSD)
     time_log = np.log10(time)
 
     # Find best interval for slope estimation
     scores = np.zeros(shape=((time[-5] - time[5]) // 10000,4)) # This is error-prone
-    # logging.debug(f'Scores shape: {scores.shape}')
     for idx, start_time in enumerate(range(time[5],time[-5],10000)):
-    # for idx, start_time in enumerate(range(10_000,3_010_000,10000)):
         mask = np.where((time > start_time) & (time < start_time + 1e6))[0]
         start = mask.min()
         end = mask.max()
@@ -95,7 +101,6 @@ def find_best_interval(moltype_obj):
     scores_sorted = scores[np.argsort(scores[:,2])] # Sort scores by slope
     closest_to_one_idx = np.argmin(np.abs((scores_sorted[:,2] - 1))) # Find index of the slope closest to 1
     best_time_interval = scores_sorted[closest_to_one_idx]
-    print(best_time_interval)
     
     start_time = int(best_time_interval[0])
     end_time = int(best_time_interval[1])
